f62af0d9c49313e0b8cb54946ba903456c255c99
Clay Teeter <clay.teeter@maalka.com>
2018-09-17 13:09:15 +0200

    propertly publishes

--- project/Common.scala
+++ project/Common.scala
@@ -4,11 +4,9 @@ import sbt.Keys._
 object Common {
     val playVersion = System.getProperty("play.version", "2.6.15")
     val settings: Seq[Setting[_]] = Seq(
-        version := System.getProperty("version", "maalka-SNAPSHOT"),
+        version := System.getProperty("version", "maalka-2.6.0"),
         scalaVersion :=System.getProperty("scala.version", "2.12.6"),
-        crossScalaVersions := Seq("2.12.6", "2.11.12"),
-        publishTo := Some("Artifactory Realm" at "https://jfrog.maalka.com/artifactory/libs-release-local"),
-        credentials += Credentials(Path.userHome / ".ivy2" / ".credentials")
+        crossScalaVersions := Seq("2.12.6", "2.11.12")
     )
     val nonPublishSettings = settings ++ Seq(
         publish := { }
